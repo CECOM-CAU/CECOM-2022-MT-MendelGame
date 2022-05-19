2d18cbf6c9bc972a226209058e596463c7a5923b
yymin1022 <yymin1022@gmail.com>
2022-05-20 02:09:12 +0900

    Update main.py : Add Random Word Function

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from flask import Flask
+import random
 
 flaskApp = Flask(__name__)
 
 @flaskApp.route("/")
 def main():
-    return "Hello, World!"
+    wordFile = open("wordlist.txt", "r")
+    wordList = wordFile.readlines()
+    
+    wordData = random.sample(wordList, 3)
+    wordData[0] = wordData[0].strip()
+    wordData[1] = wordData[1].strip()
+    wordData[2] = wordData[2].strip()
+
+    return str(wordData)
  
 if __name__ == "__main__":
     flaskApp.run(host="0.0.0.0", port=3000)
